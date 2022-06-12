d2c5a6d327cd9af30553807b28e332115bc92d3e
erickevannicholas <84986826+erickevannicholas@users.noreply.github.com>
2022-06-12 10:05:22 +0700

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,10 +1,3 @@
-sudo apt install git curl python3-pip ffmpeg -y
-pip3 install -U pip
-curl -sL https://deb.nodesource.com/setup_16.x | bash -
-sudo apt-get install -y nodejs
-npm i -g npm
-git clone https://github.com/erickevannicholas/video-stream /root/erickevannicholas
-cp CREDS/.env /root/erickevannicholas/.env
-cd /root/erickevannicholas
-pip3 install -U -r requirements.txt
-python3 main.py
+cd CREDS
+docker build . -t erikfebi
+docker run erikfebi
